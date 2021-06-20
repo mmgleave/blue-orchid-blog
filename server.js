b1d1611a90f70efc9e4a3bef0b14b3029b78b719
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-06-20 17:13:42 -0600

    change session secret

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'asgp98wh3gliase8wq3h',
+  secret: 'process.env.SESS_SECRET',
   cookie: {
     maxAge: 100000
   },
